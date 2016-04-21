c41c322167675b36bc7c6e4072c82e5f7ab2d891
Leo Fontes <leofontes@gmail.com>
2016-04-21 16:15:08 -0400

    Tested fragments

--- app/src/main/java/jhu/voiceit/MainActivity.java
+++ app/src/main/java/jhu/voiceit/MainActivity.java
@@ -1,15 +1,16 @@
 package jhu.voiceit;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fragmentManager;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setNavigationIcon(R.drawable.ic_search_white_24dp);
         setSupportActionBar(toolbar);
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragment = new RecordActivity();
+        fragmentManager.beginTransaction().replace(R.id.mainActivityRelLayout, fragment).commit();
     }
 
     @Override
