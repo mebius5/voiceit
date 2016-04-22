0a73dbf51680d7673db0c7a14acf8e08a6706094
Leo Fontes <leofontes@gmail.com>
2016-04-22 19:34:56 -0400

    Created and prepared NotificationsFragment, going to start working on it now

---
+++ app/src/main/java/layout/NotificationsFragment.java
@@ -0,0 +1,38 @@
+package layout;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jhu.voiceit.R;
+
+
+public class NotificationsFragment extends Fragment {
+    public final static String FRAGMENTNAME = "NotificationsFragment";
+    private final String fragmentName = FRAGMENTNAME;
+
+    public NotificationsFragment() {
+        // Required empty public constructor
+    }
+
+    public static NotificationsFragment newInstance(String param1, String param2) {
+        NotificationsFragment fragment = new NotificationsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        return view;
+    }
+
+}
