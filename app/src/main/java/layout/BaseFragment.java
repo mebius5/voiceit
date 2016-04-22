a36df0a594a11ca006a48280195118c11f4cb924
grady_xiao <xiao.grady@gmail.com>
2016-04-22 14:36:55 -0400

    HomeFeedFragment

---
+++ app/src/main/java/layout/BaseFragment.java
@@ -0,0 +1,10 @@
+package layout;
+
+import android.app.Fragment;
+
+/**
+ * Created by GradyXiao on 4/22/16.
+ */
+public abstract class BaseFragment extends Fragment {
+    public abstract String getFragmentName();
+}
