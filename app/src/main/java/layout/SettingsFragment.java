d5b1eedfcf744f8878b45094230f43fdf741969b
Sarah Sukardi <sukardi@jhu.edu>
2016-04-23 19:40:07 -0400

    minor

--- app/src/main/java/layout/SettingsFragment.java
+++ app/src/main/java/layout/SettingsFragment.java
@@ -60,7 +60,6 @@ public class SettingsFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        //hi
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
