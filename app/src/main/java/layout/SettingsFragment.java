8e16cbf90ccabee2fa3adf4e049598bfc5bff50e
Sarah Sukardi <sukardi@jhu.edu>
2016-04-23 19:39:40 -0400

    changeusername page and stuff implemented (i think?)

--- app/src/main/java/layout/SettingsFragment.java
+++ app/src/main/java/layout/SettingsFragment.java
@@ -60,6 +60,8 @@ public class SettingsFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //hi
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
